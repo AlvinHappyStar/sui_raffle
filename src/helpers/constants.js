175d261588856201fcda2feedaf530911747cf50
root <root@dream.com>
2022-12-20 14:14:45 +0300

    final

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
 export const suiRpcUrl = 'https://fullnode.devnet.sui.io:443'
-export const gameAddress = '0xc8e7b68631dbb21ada668a45538d13f91b09cbab';
-export const casinoAddress ='0x32115df8b5985009db2cfe84fd7719ca7b7a5e68';
-export const moduleAddress = '0x299a967315fb666628af700158ac0fdbd580f4fe';
+export const gameAddress = '0x27f92734439fc4c3829af6ecbbd1eff0510dc9f5';
+export const casinoAddress ='0x6ef22b154dafc2afa5291c7c7d58ac45b93a8c8d';
+export const moduleAddress = '0xf6cf14e49d0d029353d0e2b828567fe2a2716670';
 export const moduleName = 'Base';
 export const localStorageKeys = {
     address: 'user_sui_address',
